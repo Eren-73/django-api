a4cdbd6b5c10cbe6e122a23bb7ffd4a16be2e7b1
Eren-73 <alhousein73@gmail.com>
2025-05-13 19:37:47 +0000

    modifs des dates

--- devise/urls.py
+++ devise/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from devise import views
 
 urlpatterns = [
+  path("" , views.dashboard, name = "index"),
   path("days=<int:days_range>&currencies=<str:currencies>/", views.dashboard, name="index"),]
--- devise/views.py
+++ devise/views.py
@@ -5,5 +5,8 @@ import  api
 
 def dashboard(request, days_range = 5  , currencies = "CAD"):
     days, rates = api.get_rates(currencies=currencies.split(","), days=days_range)
-    return render(request,"index.html",context={"data": rates["CHF"],
+
+
+    
+    return render(request,"index.html",context={"data": rates[currencies],
                                                 "days_labels": days})
